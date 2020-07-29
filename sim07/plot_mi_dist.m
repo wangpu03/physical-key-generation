77158c57174931edcadfb5cd1ba10de339946793
Pu Wang <wangpu03@gmail.com>
2020-07-29 11:25:44 +0800

    update plot_mi_eth

--- sim07/plot_mi_dist.m
+++ sim07/plot_mi_dist.m
@@ -21,7 +21,6 @@ rand_ints_gen = randi(2,n_fft*mod_order*K,1)-1;
 save data_input.txt -ascii rand_ints_gen
 rand_ints = load("data_input.txt");
 data_ofdm = ofdm_module(rand_ints, mod_method, n_fft, n_cp, c_flag);
-
 num_sim = 50000;
 snr = 10:3:40;
 v1 = zeros(num_sim, length(snr));
@@ -30,12 +29,11 @@ ve1 = zeros(num_sim, length(snr));
 ve2 = zeros(num_sim, length(snr));
 
 %%
-cor = 0;
-d_e1 = 1;
-%[v_y1_cp, v_y2_cp, v_ye1_cp, v_ye2_cp] = skr_snr(data_ofdm, n_ofdm, n_cp ,n_frame, cor, d_e1, snr)
+d = [8 7 3];
+taps = [8 7 3];
 for i = 1:length(snr)
 for j = 1:num_sim
-    [v1(j,i),v2(j,i),ve1(j,i),ve2(j,i)] = skr_snr(data_ofdm, n_ofdm, n_cp ,K, cor, d_e1, snr(i));
+    [v1(j,i),v2(j,i),ve1(j,i),ve2(j,i)] = mi_dist(data_ofdm, n_ofdm, n_cp ,K, d, taps, snr(i));
 end
 end
 
@@ -64,24 +62,22 @@ for i = 1:length(snr)
     cmi_ve1(i) = cmi(v1_bit(:,i),v2_bit(:,i),ve1_bit(:,i));
 end
 
-v1_0 = v1;
-v2_0 = v2;
-ve1_0 = ve1;
-ve2_0 = ve2;
-mi_bit_0 = mi_bit;
-cmi_ve2_0 = cmi_ve2;
-cmi_ve1_0 = cmi_ve1;
+v1_873 = v1;
+v2_873 = v2;
+ve1_873 = ve1;
+ve2_873 = ve2;
+mi_bit_873 = mi_bit;
+cmi_ve2_873 = cmi_ve2;
+cmi_ve1_873 = cmi_ve1;
 
 %%
-cor = 0.3;
-d_e1 = 1;
-%[v_y1_cp, v_y2_cp, v_ye1_cp, v_ye2_cp] = skr_snr(data_ofdm, n_ofdm, n_cp ,n_frame, cor, d_e1, snr)
+d = [12 7 3];
+taps = [12 7 3];
 for i = 1:length(snr)
 for j = 1:num_sim
-    [v1(j,i),v2(j,i),ve1(j,i),ve2(j,i)] = skr_snr(data_ofdm, n_ofdm, n_cp ,K, cor, d_e1, snr(i));
+    [v1(j,i),v2(j,i),ve1(j,i),ve2(j,i)] = mi_dist(data_ofdm, n_ofdm, n_cp ,K, d, taps, snr(i));
 end
 end
-
 mean_v1 = repmat(mean(v1), num_sim,1);
 mean_v2 = repmat(mean(v2), num_sim,1);
 mean_ve1 = repmat(mean(ve1), num_sim,1);
@@ -107,21 +103,21 @@ for i = 1:length(snr)
     cmi_ve1(i) = cmi(v1_bit(:,i),v2_bit(:,i),ve1_bit(:,i));
 end
 
-v1_3 = v1;
-v2_3 = v2;
-ve1_3 = ve1;
-ve2_3 = ve2;
-mi_bit_3 = mi_bit;
-cmi_ve2_3 = cmi_ve2;
-cmi_ve1_3 = cmi_ve1;
+v1_1273 = v1;
+v2_1273 = v2;
+ve1_1273 = ve1;
+ve2_1273 = ve2;
+mi_bit_1273 = mi_bit;
+cmi_ve2_1273 = cmi_ve2;
+cmi_ve1_1273 = cmi_ve1;
+
 
 %%
-cor = 0.6;
-d_e1 = 1;
-%[v_y1_cp, v_y2_cp, v_ye1_cp, v_ye2_cp] = skr_snr(data_ofdm, n_ofdm, n_cp ,n_frame, cor, d_e1, snr)
+d = [12 7 6];
+taps = [12 7 6];
 for i = 1:length(snr)
 for j = 1:num_sim
-    [v1(j,i),v2(j,i),ve1(j,i),ve2(j,i)] = skr_snr(data_ofdm, n_ofdm, n_cp ,K, cor, d_e1, snr(i));
+    [v1(j,i),v2(j,i),ve1(j,i),ve2(j,i)] = mi_dist(data_ofdm, n_ofdm, n_cp ,K, d, taps, snr(i));
 end
 end
 
@@ -141,6 +137,7 @@ mi_bit = zeros(1,length(snr));
 for i = 1:length(snr)
     mi_bit(i) = mi(v1_bit(:,i),v2_bit(:,i));
 end
+
 for i = 1:length(snr)
     cmi_ve2(i) = cmi(v1_bit(:,i),v2_bit(:,i),ve2_bit(:,i));
 end
@@ -149,50 +146,62 @@ for i = 1:length(snr)
     cmi_ve1(i) = cmi(v1_bit(:,i),v2_bit(:,i),ve1_bit(:,i));
 end
 
-v1_6 = v1;
-v2_6 = v2;
-ve1_6 = ve1;
-ve2_6 = ve2;
-mi_bit_6 = mi_bit;
-cmi_ve2_6 = cmi_ve2;
-cmi_ve1_6 = cmi_ve1;
+v1_1276 = v1;
+v2_1276 = v2;
+ve1_1276 = ve1;
+ve2_1276 = ve2;
+mi_bit_1276 = mi_bit;
+cmi_ve2_1276 = cmi_ve2;
+cmi_ve1_1276 = cmi_ve1;
 
-% plot(snr,mi_bit_0,'y-+','LineWidth',1.5);
-% hold on;
-% plot(snr,mi_bit_3,'y-+','LineWidth',1.5);
-% hold on;
-% plot(snr,mi_bit_6,'y-+','LineWidth',1.5);
-% hold on;
-plot(snr,cmi_ve2_0,'k-o','LineWidth',1.5);
+plot(snr, mi_bit_873,'r-o','LineWidth',1.5);
 hold on;
-plot(snr,cmi_ve1_0,'k--x','LineWidth',1.5);
-hold on;
-plot(snr,cmi_ve2_3,'r-^','LineWidth',1.5);
-hold on;
-plot(snr,cmi_ve1_3,'r--v','LineWidth',1.5);
-hold on;
-plot(snr,cmi_ve2_6,'b-s','LineWidth',1.5);
-hold on;
-plot(snr,cmi_ve1_6,'b--d','LineWidth',1.5);
+plot(snr, mi_bit_1273,'k-x','LineWidth',1.5);
+plot(snr, mi_bit_1276,'b-*','LineWidth',1.5);
+plot(snr, cmi_ve2_873,'r--s','LineWidth',1.5);
+plot(snr, cmi_ve2_1273,'k--^','LineWidth',1.5);
+plot(snr, cmi_ve2_1276,'b--v','LineWidth',1.5);
+plot(snr, mi_bit_873-cmi_ve2_873,'r-.v','LineWidth',1.5);
+plot(snr, mi_bit_1273-cmi_ve2_1273,'k-.s','LineWidth',1.5);
+plot(snr, mi_bit_1276-cmi_ve2_1276,'b-.d','LineWidth',1.5);
+hold off;
 grid on;
-axis([10 40 0.27 0.9])
-legend('cor = 0, ve2','cor = 0, ve1','cor = 0.3, ve2','cor = 0.3, ve1','cor = 0.6, ve2','cor = 0.6, ve1');
-ylabel('Secret Key Rate (bit)');
+
+axis([10 40 0 1.1])
+txt = {'Red: d_1 = 8, d_2 = 7, d_{12} = 3 ','Black: d_1 = 12, d_2 = 7, d_{12} = 3','Blue: d_1 = 12, d_2 = 7, d_{12} = 6'};
+annotation('textbox',[0.15,0.74,0.35,0.165],'LineStyle','-','LineWidth',1,'String',txt)
+
+elps1 = annotation('ellipse',[0.800 0.715 0.0500 0.0500]);
+elps1.LineWidth = 1.5;
+elps1.Color = '#D95319';
+
+x = [0.770,0.820];
+y = [0.57,0.715];
+a1 = annotation('textarrow',x, y,'String','Secret key rate');
+a1.Color = 'red';
+a1.FontSize = 12;
+a1.LineWidth = 1.5;
+
+elps2 = annotation('ellipse',[0.6500 0.75 0.0500 0.0500]);
+elps2.LineWidth = 1.5;
+elps2.Color = '#D95319';
+
+x2 = [0.65,0.6700];
+y2 = [0.65,0.75];
+a2 = annotation('textarrow',x2, y2,'String','Mutual information');
+a2.Color = 'red';
+a2.FontSize = 12;
+a2.LineWidth = 1.5;
+
+elps3 = annotation('ellipse',[0.800 0.117 0.0500 0.0500]);
+elps3.LineWidth = 1.5;
+elps3.Color = '#D95319';
+x3 = [0.77,0.830];
+y3 = [0.25,0.17];
+a3 = annotation('textarrow',x3, y3,'String','Leak information');
+a3.Color = 'red';
+a3.FontSize = 12;
+a3.LineWidth = 1.5;
+%ylabel('Information (bit)');
 xlabel('SNR (dB)');
 
-plot(snr,mi_bit_0-cmi_ve2_0,'k-o','LineWidth',1.5);
-hold on;
-plot(snr,mi_bit_0-cmi_ve1_0,'k--x','LineWidth',1.5);
-hold on;
-plot(snr,mi_bit_3-cmi_ve2_3,'r-^','LineWidth',1.5);
-hold on;
-plot(snr,mi_bit_3-cmi_ve1_3,'r--v','LineWidth',1.5);
-hold on;
-plot(snr,mi_bit_6-cmi_ve2_6,'b-s','LineWidth',1.5);
-hold on;
-plot(snr,mi_bit_6-cmi_ve1_6,'b--d','LineWidth',1.5);
-grid on;
-axis([10 40 0 0.52])
-legend('cor = 0, ve2','cor = 0, ve1','cor = 0.3, ve2','cor = 0.3, ve1','cor = 0.6, ve2','cor = 0.6, ve1');
-ylabel('Leak Information (bit)');
-xlabel('SNR (dB)');
---
+++ sim07/plot_mi_eth.m
@@ -0,0 +1,195 @@
+% plot_mi_eth.m
+
+clc;
+clear;
+
+d = [10 7 3];
+alpha = 0.3 + 1i*0.4;
+
+lamda = 3;
+h = 5*d.^(-lamda/2);
+
+rho1 =0:0.01:1;
+rho2 =0:0.01:1;
+
+%%
+snr = 30;
+pt = 1;
+P = 4*pt*abs(alpha)*h(1)*h(2)*h(3);
+pw_noise = pt/10^(snr/10);
+
+E_th = 0:0.001:0.1;
+mi_max = zeros(1,length(E_th));
+mi_diag_max = zeros(1,length(E_th));
+for j = 1:length(E_th)
+    mi = zeros(length(rho1),length(rho2));
+    for i = 1:length(rho1)
+        E1 = 0.5 * pt * (1-rho1(i))* h(1)^2;
+        if E1 <  E_th(j)
+            break;
+        end
+    end
+    num_rho1 = i-1;
+    
+    for i = 1:length(rho2)
+        E2 = 0.5 * pt * (1-rho2(i))* h(2)^2;
+        if E2 <  E_th(j)
+            break;
+        end
+    end
+    num_rho2 = i-1;
+    
+    for i = 1:num_rho1
+        for k = 1:num_rho2
+            mi(i,k) = 1/2*log10(1+rho1(i)^2*rho2(k)^2*P/(rho1(i)^2*pw_noise+rho2(k)^2*pw_noise+pw_noise*pw_noise/P));
+        end
+    end
+    
+    mi_max (j) = max(max(mi)');
+    mi_diag_max(j) = max(diag(mi));
+end
+
+
+%%
+snr = 25;
+pt = 1;
+P = 4*pt*abs(alpha)*h(1)*h(2)*h(3);
+pw_noise = pt/10^(snr/10);
+
+E_th = 0:0.001:0.1;
+mi_25_max = zeros(1,length(E_th));
+mi_diag_25_max = zeros(1,length(E_th));
+for j = 1:length(E_th)
+    mi = zeros(length(rho1),length(rho2));
+    for i = 1:length(rho1)
+        E1 = 0.5 * pt * (1-rho1(i))* h(1)^2;
+        if E1 <  E_th(j)
+            break;
+        end
+    end
+    num_rho1 = i-1;
+    
+    for i = 1:length(rho2)
+        E2 = 0.5 * pt * (1-rho2(i))* h(2)^2;
+        if E2 <  E_th(j)
+            break;
+        end
+    end
+    num_rho2 = i-1;
+    
+    for i = 1:num_rho1
+        for k = 1:num_rho2
+            mi(i,k) = 1/2*log10(1+rho1(i)^2*rho2(k)^2*P/(rho1(i)^2*pw_noise+rho2(k)^2*pw_noise+pw_noise*pw_noise/P));
+        end
+    end
+    
+    mi_25_max (j) = max(max(mi)');
+    %mi_diag_35_max(j) = max(diag(mi));
+end
+
+%%
+snr = 30;
+pt = 1.5;
+P = 4*pt*abs(alpha)*h(1)*h(2)*h(3);
+pw_noise = 1/10^(snr/10);
+
+E_th = 0:0.001:0.1;
+mi_2_max = zeros(1,length(E_th));
+mi_diag_2_max = zeros(1,length(E_th));
+for j = 1:length(E_th)
+    mi = zeros(length(rho1),length(rho2));
+    for i = 1:length(rho1)
+        E1 = 0.5 * pt * (1-rho1(i))* h(1)^2;
+        if E1 <  E_th(j)
+            break;
+        end
+    end
+    num_rho1 = i-1;
+    
+    for i = 1:length(rho2)
+        E2 = 0.5 * pt * (1-rho2(i))* h(2)^2;
+        if E2 <  E_th(j)
+            break;
+        end
+    end
+    num_rho2 = i-1;
+    
+    for i = 1:num_rho1
+        for k = 1:num_rho2
+            mi(i,k) = 1/2*log10(1+rho1(i)^2*rho2(k)^2*P/(rho1(i)^2*pw_noise+rho2(k)^2*pw_noise+pw_noise*pw_noise/P));
+        end
+    end
+    
+    mi_2_max (j) = max(max(mi)');
+    %mi_diag_35_max(j) = max(diag(mi));
+end
+
+num_max = sum(mi_max ~= 0);
+num_diag_max = sum(mi_diag_max ~= 0);
+num_25_max = sum(mi_25_max ~= 0);
+num_2_max = sum(mi_2_max ~= 0);
+% 
+plot([E_th(1:num_max),E_th(num_max)], [mi_max(1:num_max),0],'-*','LineWidth',1);
+hold on;
+plot(E_th(1:num_diag_max), mi_diag_max(1:num_diag_max),'-s','LineWidth',1);
+hold on;
+plot(E_th(1:num_25_max), mi_25_max(1:num_25_max),'-v','LineWidth',1);
+hold on;
+plot([E_th(1:num_2_max),E_th(num_2_max)], [mi_2_max(1:num_2_max),0],'-o','LineWidth',1);
+hold on;
+% % plot(E_th, mi_diag_max_2,'LineWidth',1.5);
+
+area1 = area([0,E_th(4)],[mi_max(4),mi_max(4)], 'LineStyle','none');
+plot([0,E_th(4)],[mi_max(4),mi_max(4)],'LineStyle','--','LineWidth',1.5,'Color','#0072BD');
+plot([E_th(4),E_th(4)],[0,mi_max(4)],'LineStyle','--','LineWidth',1.5,'Color','#0072BD');
+
+area2 = area([0,E_th(9)],[mi_max(9),mi_max(9)], 'LineStyle','none');
+plot([0,E_th(9)],[mi_max(9),mi_max(9)],'LineStyle','--','LineWidth',1.5,'Color','#77AC30');
+plot([E_th(9),E_th(9)],[0,mi_max(9)],'LineStyle','--','LineWidth',1.5,'Color','#77AC30');
+
+area3 = area([0,E_th(12)],[mi_max(12),mi_max(12)], 'LineStyle','none');
+plot([0,E_th(12)],[mi_max(12),mi_max(12)],'LineStyle','--','LineWidth',1.5,'Color','#A2142F');
+plot([E_th(12),E_th(12)],[0,mi_max(12)],'LineStyle','--','LineWidth',1.5,'Color','#A2142F');
+
+area1.FaceColor = '#0072BD';
+area2.FaceColor = '#77AC30';
+area3.FaceColor = '#A2142F';
+area1.FaceAlpha = 0.1;
+area2.FaceAlpha = 0.1;
+area3.FaceAlpha = 0.1;
+grid on;
+
+dim = [0.003/0.0127 0.725/1.05 0.0500 0.0500];
+a1 = annotation('textbox',dim,'String','K1');
+a1.Color = 'red';
+a1.FontSize = 12;
+a1.LineStyle = 'none';
+
+dim = [0.003/0.0068 0.725/1.476 0.0500 0.0500];
+a2 = annotation('textbox',dim,'String','K2');
+a2.Color = 'red';
+a2.FontSize = 12;
+a2.LineStyle = 'none';
+
+dim = [0.003/0.0054 0.725/3.1 0.0500 0.0500];
+a3 = annotation('textbox',dim,'String','K3');
+a3.Color = 'red';
+a3.FontSize = 12;
+a3.LineStyle = 'none';
+
+
+
+
+
+
+axis([0 0.02 0 1.05]);
+ylabel('Mutual information (bit)');
+xlabel('E_{th}');
+legend('p_t = 1, p_n =0.001','p_t = 1, p_n =0.001 (\rho_1 = \rho_2)','p_t = 1, p_n =0.003','p_t = 1.5, p_n =0.001')
+
+%mesh(rho1,rho2,mi);
+
+
+
+
+
